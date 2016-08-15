268841195a5f60d178cc104050672279b73b10fd
Velocitypulse <velocitypulse@outlook.fr>
2016-08-15 02:49:52 +0200

    avencement

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/10 14:03:30 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/08/12 22:21:30 by                  ###   ########.fr       */
+/*   Updated: 2016/08/15 02:12:07 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,28 @@
 # include <stdarg.h>
 # include "../libft/libft.h"
 
-int		ft_printf(const char *format, ...);
+# define CONV_UP_S		0x1
+# define CONV_UP_C		0x2
+# define CONV_UP_D		0x3
+# define CONV_UP_O		0x4
+# define CONV_UP_X		0x5
+# define CONV_UP_U		0x6
+
+# define CONV_LO_S		0x7
+# define CONV_LO_C		0x8
+# define CONV_LO_D		0x9
+# define CONV_LO_O		0xA
+# define CONV_LO_X		0xB
+# define CONV_LO_U		0xC
+
+# define CONV_P			0xD
+# define CONV_PERCENT	0xE
+
+typedef struct	s_data
+{
+	void	(*fct[15])();
+}				t_data;
+
+int				ft_printf(const char *format, ...);
 
 #endif
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,20 +6,62 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/10 14:03:52 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/08/12 22:35:39 by                  ###   ########.fr       */
+/*   Updated: 2016/08/15 02:47:47 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+
+
+void	ft_init_data(t_data *data)
+{
+//	data->fct[CONV_LO_S] = 
+/*	data->fct[CONV_LO_D] = 
+	data->fct[CONV_LO_U] = 
+	data->fct[CONV_UP_U] = 
+	data->fct[CONV_LO_C] = 
+	data->fct[CONV_LO_X] = 
+	data->fct[CONV_UP_X] = 
+	data->fct[CONV_LO_O] = 
+	data->fct[CONV_UP_O] = 
+	data->fct[CONV_UP_S] = 
+	data->fct[CONV_UP_C] = 
+	data->fct[CONV_PERCENT] = 
+*/
+}
+
+int		parse_format(const char *format, int *index)
+{
+	return (0);
+}
+
 int		ft_printf(const char *format, ...)
 {
 	va_list		ap;
+	int			index;
+	t_data		data;
+	int			ret;
+
+	index = -1;
+	ret	 = 0;
+
+	data.fct[2] = &ft_putstr;
+	data.fct[1] = &ft_putnbr;
+//	data.fct[1](1);
+//	data.fct[2]("lol");
 
 	va_start(ap, format);
-	ft_putnbr(va_arg(ap, int)); BN
-	ft_putnbr(va_arg(ap, int)); BN
-	ft_putnbr(va_arg(ap, int)); BN
+	while (format[++index])
+	{
+		if (format[index] == '%')
+			ret += parse_format(&format[index + 1], &index);
+		else
+		{
+			ft_putchar(format[index]);
+			ret++;
+		}
+	}
 	va_end(ap);
 	return (0);
 }
