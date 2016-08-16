be27c341335e7bbe297f3daf9e8aa0f1d09bd567
Velocitypulse <velocitypulse@outlook.fr>
2016-08-16 17:03:19 +0200

    reset du projet

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/10 14:03:30 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/08/16 01:11:48 by                  ###   ########.fr       */
+/*   Updated: 2016/08/16 17:02:49 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,6 @@
 # include <stdarg.h>
 # include "../libft/libft.h"
 
-# define CONV_UP_S		0x1
-# define CONV_UP_C		0x2
-# define CONV_UP_D		0x3
-# define CONV_UP_O		0x4
-# define CONV_UP_X		0x5
-# define CONV_UP_U		0x6
-
-# define CONV_LO_S		0x7
-# define CONV_LO_C		0x8
-# define CONV_LO_D		0x9
-# define CONV_LO_O		0xA
-# define CONV_LO_X		0xB
-# define CONV_LO_U		0xC
-
-# define CONV_P			0xD
-# define CONV_PERCENT	0xE
-
 typedef struct	s_specify
 {
 	int		sharp;
@@ -42,19 +25,13 @@ typedef struct	s_specify
 
 typedef struct	s_data
 {
-	void		(*fct_conv[14])();
 	va_list		*ap;
 	int			ret;
 	char		*format;
 	t_specify	spec;
 }				t_data;
 
-void			call_putstr(t_data *data);
-void			call_putnbr(t_data *data);
-void			call_putbase(t_data *data, int base);
-
 int				ft_printf(const char *format, ...);
-void			init_fct_conv(t_data *data);
 int				parse_conversion(t_data *data, char type);
 
 #endif
--- srcs/select_conversion.c
+++ srcs/select_conversion.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/15 16:45:18 by                   #+#    #+#             */
-/*   Updated: 2016/08/16 01:08:21 by                  ###   ########.fr       */
+/*   Updated: 2016/08/16 17:02:14 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,12 @@
 static int		conversion_a(t_data *data, char type)
 {
 	if (type == 's')
-		data->fct_conv[CONV_LO_S](data);
 	else if (type == 'd' || type == 'i')
-		data->fct_conv[CONV_LO_D](data);
 	else if (type == 'D')
-		data->fct_conv[CONV_UP_D](data);
 	else if (type == 'u')
-		data->fct_conv[CONV_LO_U](data, 8);
 	else if (type == 'U')
-		data->fct_conv[CONV_UP_U](data, 8);
 	else if (type == 'c')
-		data->fct_conv[CONV_LO_C](data);
 	else if (type == 'x')
-		data->fct_conv[CONV_LO_X](data);
 	else
 		return (0);
 	return (1);
@@ -36,19 +29,12 @@ static int		conversion_a(t_data *data, char type)
 static int		conversion_b(t_data *data, char type)
 {
 	if (type == 'X')
-		data->fct_conv[CONV_UP_X](data);
 	else if (type == 'o')
-		data->fct_conv[CONV_LO_O](data);
 	else if (type == 'O')
-		data->fct_conv[CONV_UP_O](data);
 	else if (type == 'S')
-		data->fct_conv[CONV_UP_S](data);
 	else if (type == 'C')
-		data->fct_conv[CONV_UP_C](data);
 	else if (type == '%')
-		data->fct_conv[CONV_PERCENT](data);
 	else if (type == 'p')
-		data->fct_conv[CONV_P](data);
 	else
 		return (0);
 	return (1);
