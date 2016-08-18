45720e675c19cd39db185590801be27c31016bd5
Velocitypulse <velocitypulse@outlook.fr>
2016-08-18 17:14:58 +0200

    prototype de call_nbr termine

--- srcs/call_putnbr.c
+++ srcs/call_putnbr.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/16 18:04:16 by                   #+#    #+#             */
-/*   Updated: 2016/08/17 16:57:10 by                  ###   ########.fr       */
+/*   Updated: 2016/08/18 17:14:31 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,13 @@
 void	call_putnbr(t_data *data)
 {
 	long long	n;
+	int			lenght;
 
-	if (data->spec.lenght != LENGHT_DEFAULT)
-		n = get_arg_lenght(data->ap, data->spec.lenght);
+	lenght = data->spec.lenght;
+	if (lenght != LENGHT_DEFAULT)
+		n = va_arg(*data->ap, int);
 	else
-		n = get_arg_conv(data->ap, data->spec.)
+		n = data->fct_lenght[lenght](data);
+	data->ret = ft_nblen(n);
+	ft_putnbr(n);
 }
