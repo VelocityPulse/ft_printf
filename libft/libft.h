fa648d589fde2ad9ea060a452ec0df986a1e804a
Velocitypulse <velocitypulse@outlook.fr>
2016-08-23 22:37:42 +0200

    mise a jour de la libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/21 12:20:08 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/08/21 02:07:22 by                  ###   ########.fr       */
+/*   Updated: 2016/08/23 22:30:43 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -137,7 +137,7 @@ t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 ** private functions
 */
 
-void			ft_putstrlen(char const *s, size_t len);
+void			ft_putnstr(char const *s, size_t len);
 void			ft_putnbr_l(long n);
 void			ft_putnbr_ll(long long n);
 int				ft_nblen(int n);
