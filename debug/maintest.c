360bc3a919960158a5927dd192f1c38e94b8550c
Velocitypulse <velocitypulse@outlook.fr>
2016-08-26 02:11:07 +0200

    ./debug/ : modif du makefile

--- debug/maintest.c
+++ debug/maintest.c
@@ -6,12 +6,12 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/12 21:58:22 by                   #+#    #+#             */
-/*   Updated: 2016/08/22 23:42:53 by                  ###   ########.fr       */
+/*   Updated: 2016/08/26 02:08:31 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "../includes/ft_printf.h"
-#include "blibft.h"
+#include "../includes/ft_printf.h"
+//#include "blibft.h"
 #include <wchar.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -21,12 +21,16 @@ int main()
 {
 	int n1;
 	int n2;
+	char mask[32];
 
 
-	n1 = ft_printf("%hhO, %O", 0, USHRT_MAX + 2);
+	n1 = ft_printf("%S", L"Á±≥");
+
 	BN
-	n2 = printf("%hhO, %O", 0, USHRT_MAX + 2);
-	printf("\n\n%d\n%d\n", n1, n2);
+	printf("%d\n", n1);
 	return (0);
 }
+
+
+//	ft_printf("%S\n", L"😆 😎 😵 😗 😈 🐻 💓 🌼");
 
