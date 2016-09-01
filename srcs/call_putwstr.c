9d60e1b7d189af6c541138fd0371b98a3ddc7e94
Velocitypulse <velocitypulse@outlook.fr>
2016-09-01 17:50:18 +0200

    debut de preicision sur S

--- srcs/call_putwstr.c
+++ srcs/call_putwstr.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/24 00:45:49 by                   #+#    #+#             */
-/*   Updated: 2016/08/29 22:26:59 by                  ###   ########.fr       */
+/*   Updated: 2016/09/01 17:47:19 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void			call_putwstr(t_data *data)
 		str = L"(null)";
 	data->spec.nb_len = ft_strwlen(str);
 	if (spec->dot == true && spec->dot_value < spec->nb_len)
-		spec->nb_len = spec->dot_value;
+		spec->nb_len =  ft_strnwlen(str, spec->dot_value);
 	before_printing_s(data, &data->spec);
 	data->ret += ft_putnwstr(str, spec->nb_len);
 	after_printing_s(data, &data->spec);
